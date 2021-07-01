73b5e24dc874e36164385ade2816083ff5544773
Federico Caccia <federicoagustincaccia@gmail.com>
2021-07-01 18:24:15 -0300

    dummy

--- components/InputBox.js
+++ components/InputBox.js
@@ -16,6 +16,9 @@ const InputBox = () => {
   const sendPost = (e) => {
     e.preventDefault();
 
+    console.log("sendPost")
+    console.log(inputRef.current.value)
+
     if (!inputRef.current.value) return;
 
     db.collection("posts").add({
